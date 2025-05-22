73a249d9cd309d2e1f2abaa5e19f28feab3e49d8
Mhando-Z <Mhandosz17@gmail.com>
2025-05-22 12:22:21 +0300

    Phase Two

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="flex bg-green-50 font-roboto flex-col min-h-screen overflow-x-hidden">
+      <body className="flex bg-green-50 appearance-none font-roboto flex-col min-h-screen overflow-x-hidden">
         <ToastContainer
           position="top-right"
           autoClose={5000}
